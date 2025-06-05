188e5545d4dbbcc2338e1ef8239d0ce8c4cb4d05
dyeaman05 <danielyeaman05@gmail.com>
2025-06-04 19:38:51 -0500

    Added Raylib Libraries from quickstart-main
    Added raylib files from the quickstart-main repo. Will begin modifying example code given from repo.

--- README.md
+++
@@ -1,3 +0,0 @@
-#  C++ Tetris Project with raylib:
-
-Working on writing a tetris program from scratch to learn more about C++, external libraries, UI creation, and game design. Feel free to read and use code for inspiration and leave feedback.
---
+++ build/build_files/obj/ARM64/Debug/raylib-quickstart-main/main.d
@@ -0,0 +1,13 @@
+obj/ARM64/Debug/raylib-quickstart-main/main.o: ../../src/main.c \
+  ../external/raylib-master/src/raylib.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/14.0.3/include/stdarg.h \
+  /Library/Developer/CommandLineTools/usr/lib/clang/14.0.3/include/stdbool.h \
+  ../../include/resource_dir.h
+
+../external/raylib-master/src/raylib.h:
+
+/Library/Developer/CommandLineTools/usr/lib/clang/14.0.3/include/stdarg.h:
+
+/Library/Developer/CommandLineTools/usr/lib/clang/14.0.3/include/stdbool.h:
+
+../../include/resource_dir.h:
